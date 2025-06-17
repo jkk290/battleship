da107b104aeeac98a515e557147dccd9b6a6f804
jkk290 <158115736+jkk290@users.noreply.github.com>
2025-06-17 10:57:38 -1000

    Add handlePlayerClick and createCell function
    Implemented createCell function for generating cell coordinates and attach eventlistener for player clicks

--- src/game.js
+++ src/game.js
@@ -110,9 +110,30 @@ export function playGame() {
     return activePlayer;
   }
 
-  function handlePlayerClick() {
-    // click eventlistener for coords
-    // pass clicked on coords x and y
+  function createCell(boardContainer, boardType) {
+    const container = document.querySelector(boardContainer);
+
+    for (let i = 0; i < 10; i++) {
+      for (let j = 0; j < 10; j++) {
+        const cell = document.createElement('div');
+        cell.dataset.x = i;
+        cell.dataset.y = j;
+        cell.classList.add('cell', `${boardType}-cell`);
+
+        if (boardType === 'record') {
+          cell.addEventListener('click', handlePlayerClick)
+        }
+
+        container.appendChild(cell);
+      }
+    }
+
+  }
+
+  function handlePlayerClick(event) {
+
+    let targetX = parseInt(event.target.dataset.x);
+    let targetY = parseInt(event.target.dataset.y);
 
     playerRound(targetX, targetY);
 
